922eb33c600878759320cb6c156d05d34fabca08
Nitika Devgan <nitikadevganisnd@gmail.com>
2026-03-22 15:47:20 +0530

    added toast notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SalesChart from './Components/SalesChart';
 import { salesData } from './data/salesData';
--- src/Services/mockApi.ts
+++ src/Services/mockApi.ts
@@ -1,8 +1,12 @@
 import { SalesData } from "../types/chartTypes";
 
 export const getfetchSalesData = async (): Promise<SalesData[]> => {
-  return new Promise((resolve) => {
+  return new Promise((resolve,reject) => {
     setTimeout(() => {
+        const fail = Math.random() < 5.0; //50% fail
+        if (fail) {
+            reject(new Error("Network Error, Please try later."));
+        }else {
       resolve([
         { month: "January", revenue: 5000 },
         { month: "Feb", revenue: 2000 },
@@ -10,6 +14,7 @@ export const getfetchSalesData = async (): Promise<SalesData[]> => {
         { month: "May", revenue: 3444 },
         { month: "August", revenue: 1000 },
       ]);
+    }
     },1500);
   });
 };
