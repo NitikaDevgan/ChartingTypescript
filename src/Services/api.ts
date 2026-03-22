f54af1aa4ef34d393811e754469911f6badf52f2
Nitika Devgan <nitikadevganisnd@gmail.com>
2026-03-22 15:39:09 +0530

    using fake api call for testing

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -1,15 +1,6 @@
 import { SalesData } from "../types/chartTypes";
+import { getfetchSalesData } from "./mockApi"
 
-export const fetchSalesData = async (): Promise<SalesData[]> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve([
-        { month: "January", revenue: 5000 },
-        { month: "Feb", revenue: 2000 },
-        { month: "March", revenue: 8000 },
-        { month: "May", revenue: 3444 },
-        { month: "August", revenue: 1000 },
-      ]);
-    },1500);
-  });
-};
+export const fetchSalesData = async () : Promise<SalesData[]> => {
+  return getfetchSalesData();
+}
---
+++ src/Services/mockApi.ts
@@ -0,0 +1,15 @@
+import { SalesData } from "../types/chartTypes";
+
+export const getfetchSalesData = async (): Promise<SalesData[]> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        { month: "January", revenue: 5000 },
+        { month: "Feb", revenue: 2000 },
+        { month: "March", revenue: 8000 },
+        { month: "May", revenue: 3444 },
+        { month: "August", revenue: 1000 },
+      ]);
+    },1500);
+  });
+};
