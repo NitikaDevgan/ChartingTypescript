e3315c0d61ec916ac56a3fd38e9124d14991f0db
Nitika Devgan <nitikadevganisnd@gmail.com>
2026-04-01 22:21:04 +0530

    pie chart changes

--- src/Components/RevenuePieChart.tsx
+++ src/Components/RevenuePieChart.tsx
@@ -36,7 +36,12 @@ const RevenuePieChart = ({data} : props) => {
 </div>
 
     <PieChart width={400} height={300}>
-      <Pie data={data} dataKey="revenue" nameKey="month" fill="#8884d8" />
+      <Pie  data={data}
+  dataKey="revenue"
+  nameKey="month"
+  outerRadius={100}
+  fill="#8884d8"
+  label/>
       <Tooltip />
     </PieChart>
     </>
